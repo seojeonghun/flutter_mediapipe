38608764c2d76de99ae01621f54e5a380e2c746b
seojeonghun <ss20311@simseok.hs.kr>
2021-09-04 23:20:47 +0900

    안구건조증기능

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math' as math;
 
+import 'package:flutter/material.dart';
+import 'package:sensors/sensors.dart';
 import 'package:flutter_mediapipe/flutter_mediapipe.dart';
 import 'package:flutter_mediapipe/gen/landmark.pb.dart';
 
@@ -13,6 +16,22 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  double count = 0.0;
+  double deviceAngle = 0.0;
+
+  @override
+  void initState() {
+    accelerometerEvents.listen((AccelerometerEvent event) {
+      setState(() {
+        // valueX = (event.x * 1000).toInt() / 100;
+        // valueY = (event.y * 1000).toInt() / 100;
+        // valueZ = (event.z * 1000).toInt() / 100;
+        deviceAngle = (event.z * 1000).toInt() / 100;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,19 +39,72 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Flutter Mediapipe'),
         ),
-        body: Container(
-            child: NativeView(
-                onViewCreated: (FlutterMediapipe c) => setState(() {
-                      c.landMarksStream.listen(_onLandMarkStream);
-                      c.platformVersion.then((content) => print(content));
-                    }))),
+        body: Stack(
+          children: [
+            Positioned(
+              child: NativeView(
+                onViewCreated: (FlutterMediapipe c) => setState(
+                  () {
+                    c.landMarksStream.listen(_onLandMarkStream);
+                    c.platformVersion.then((content) => print(content));
+                  },
+                ),
+              ),
+            ),
+            Positioned(
+              child: Text(
+                "$count\n$deviceAngle",
+                style: TextStyle(color: Colors.red, fontSize: 35),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   void _onLandMarkStream(NormalizedLandmarkList landmarkList) {
-    landmarkList.landmark.asMap().forEach((int i, NormalizedLandmark value) {
-      print('Index: $i \n' + '$value');
-    });
+    //   landmarkList.landmark.asMap().forEach((int i, NormalizedLandmark value) {
+    //    // print('Index: $i \n' + '$value');
+    //   });
+
+    //   print("8번 x");
+    //   print(landmarkList.landmark.asMap()[8]!.x);
+    //   print("8번 y");
+    //   print(landmarkList.landmark.asMap()[8]!.y);
+    //   print("8번 z");
+    //   print(landmarkList.landmark.asMap()[8]!.z);
+    //   print(" ");
+
+    //   print("18번 x");
+    //   print(landmarkList.landmark.asMap()[18]!.x);
+    //   print("18번 y");
+    //   print(landmarkList.landmark.asMap()[18]!.y);
+    //   print("18번 z");
+    //   print(landmarkList.landmark.asMap()[18]!.z);
+    //   print(" ");
+    double xCal = landmarkList.landmark.asMap()[133]!.x -
+        landmarkList.landmark.asMap()[33]!.x;
+    double yCal = landmarkList.landmark.asMap()[145]!.y -
+        landmarkList.landmark.asMap()[159]!.y;
+    double result = calculateDegree(a: yCal, b: xCal);
+
+    count = result;
+
+    print(result);
   }
+
+  /// ### tan(a/b)로 사용
+  ///  - a: 분자
+  ///  - b: 분모
+  double calculateDegree({
+    required double a,
+    required double b,
+  }) {
+   
+    double degree =100 * a/b / math.pi.toInt(); // (세로/가로) * 100
+
+    return degree;
+  }
+  
 }
--- lib/flutter_mediapipe.dart
+++ lib/flutter_mediapipe.dart
@@ -1,52 +1,111 @@
 import 'dart:async';
+import 'dart:math' as math;
 
-import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
+import 'package:sensors/sensors.dart';
+import 'package:flutter_mediapipe/flutter_mediapipe.dart';
 import 'package:flutter_mediapipe/gen/landmark.pb.dart';
 
-class FlutterMediapipe {
-  final MethodChannel _methodChannel;
-  final EventChannel _eventChannel;
-
-  FlutterMediapipe()
-      : _methodChannel = MethodChannel("sotai.co/flutter_mediapipe"),
-        _eventChannel = EventChannel("sotai.co/flutter_mediapipe_event");
-
-  Future<String> get platformVersion async =>
-      await _methodChannel.invokeMethod("getPlatformVersion");
-
-  Stream<NormalizedLandmarkList> get landMarksStream async* {
-    yield* _eventChannel
-        .receiveBroadcastStream()
-        .map((buffer) => NormalizedLandmarkList.fromBuffer(buffer));
-  }
+void main() {
+  runApp(MyApp());
 }
 
-const NAMESPACE = "plugins.sotai.co/flutter_mediapipe";
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-typedef void NativeViewCreatedCallback(FlutterMediapipe controller);
+class _MyAppState extends State<MyApp> {
+  double count = 0.0;
+  double deviceAngle = 0.0;
 
-class NativeView extends StatelessWidget {
-  const NativeView({required this.onViewCreated});
-
-  final NativeViewCreatedCallback onViewCreated;
+  @override
+  void initState() {
+    accelerometerEvents.listen((AccelerometerEvent event) {
+      setState(() {
+        // valueX = (event.x * 1000).toInt() / 100;
+        // valueY = (event.y * 1000).toInt() / 100;
+        // valueZ = (event.z * 1000).toInt() / 100;
+        deviceAngle = (event.z * 1000).toInt() / 100;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return AndroidView(
-          viewType: "flutter_mediapipe/view",
-          onPlatformViewCreated: (int id) => onViewCreated(FlutterMediapipe()),
-        );
-      case TargetPlatform.fuchsia:
-      case TargetPlatform.iOS:
-      default:
-        throw UnsupportedError(
-            "Trying to use the default webview implementation for"
-            " $defaultTargetPlatform but there isn't a default one");
-    }
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Mediapipe'),
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+              child: NativeView(
+                onViewCreated: (FlutterMediapipe c) => setState(
+                  () {
+                    c.landMarksStream.listen(_onLandMarkStream);
+                    c.platformVersion.then((content) => print(content));
+                  },
+                ),
+              ),
+            ),
+            Positioned(
+              child: Text(
+                "$count\n$deviceAngle",
+                style: TextStyle(color: Colors.red, fontSize: 35),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
+  void _onLandMarkStream(NormalizedLandmarkList landmarkList) {
+    //   landmarkList.landmark.asMap().forEach((int i, NormalizedLandmark value) {
+    //    // print('Index: $i \n' + '$value');
+    //   });
+
+    //   print("8번 x");
+    //   print(landmarkList.landmark.asMap()[8]!.x);
+    //   print("8번 y");
+    //   print(landmarkList.landmark.asMap()[8]!.y);
+    //   print("8번 z");
+    //   print(landmarkList.landmark.asMap()[8]!.z);
+    //   print(" ");
+
+    //   print("18번 x");
+    //   print(landmarkList.landmark.asMap()[18]!.x);
+    //   print("18번 y");
+    //   print(landmarkList.landmark.asMap()[18]!.y);
+    //   print("18번 z");
+    //   print(landmarkList.landmark.asMap()[18]!.z);
+    //   print(" ");
+    double xCal = landmarkList.landmark.asMap()[133]!.x -
+        landmarkList.landmark.asMap()[33]!.x;
+    double yCal = landmarkList.landmark.asMap()[145]!.y -
+        landmarkList.landmark.asMap()[159]!.y;   
+    double yyCal = yCal*yCal;
+    double result = calculateDegree(a: yCal, b: xCal);
+
+    count = result;
+
+    print(result);
+  }
+
+  /// ### tan(a/b)로 사용
+  ///  - a: 분자
+  ///  - b: 분모
+  double calculateDegree({
+    required double a,
+    required double b,
+  }) {
+   
+    double degree = a  / b / math.pi; // (세로/가로) * 100
+
+    return degree;
+  }
+  
 }
